148bb25e9faaeb87ac5fbc58c6f292114c1872c2
Hadi Akram R <110667740+Hadi-Akram-Ramadhan@users.noreply.github.com>
2023-09-08 11:16:20 +0700

    My Simple HTML - JavaScript - PHP Projects

---
+++ BLOKING 20-25 FEB/MODUL 9/php/lat3.php
@@ -0,0 +1,12 @@
+<html>
+    <head><title>
+        Komentar - Unix Shell Style
+    </title></head>
+    <body>
+        <?php
+            # ini adalah skrip latihan komentar
+            # menggunakan ( # )
+            echo "Mencoba Komentar pada PPHP menggunakan Unix Shell Style."
+        ?>
+    </body>
+</html>

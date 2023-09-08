f9191f78431adafeb013d9aedc02a4839f6df31c
Hadi Akram R <110667740+Hadi-Akram-Ramadhan@users.noreply.github.com>
2023-09-08 11:21:54 +0700

    My Simple HTML - PHP Projects

---
+++ BLOKING 20-25 FEB/MODUL 9/php/lat1.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Program PHP Pertama saya</title>
+    </head>
+    <body>
+        <?php echo "Hello World!"; ?>
+    </body>
+</html>
---
+++ BLOKING 20-25 FEB/MODUL 9/php/lat7.php
@@ -0,0 +1,13 @@
+<?php
+$bool = true;
+// sebuah boolean
+$str = "foo";
+// sebuah string
+$int = 12;
+// sebuah integer
+echo "Variabel \$bool bertipe : ";
+echo gettype ($bool);
+echo "<br />";
+echo "Variabel \$str bertipe : ";
+echo gettype ($str);
+?>
